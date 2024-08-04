b46f7a22373553c07c6b8ebf904bdfe251d5d1de
Arturo Escobar <s.arturoec@gmail.com>
2024-08-03 23:41:32 -0600

    add custom scroll bar class

--- frontend/UI/src/routes/Root.jsx
+++ frontend/UI/src/routes/Root.jsx
@@ -22,24 +22,12 @@ function PageRoot() {
   const { user } = useAuth();
 
   return (
-    <div>
-      <div className="space-y-4">
-        {/* <div>
-          <h1>Aqui deberia ir un header</h1>
-          <p>Estas logeado como: {user.username}</p>
-          <p>El rol es: {user.role}</p>
-          <p>Pertenece al proceso: {user.process}</p>
-        </div>
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={handleLogout}
-        >
-          Cerrar sesion
-        </button> */}
-        <Header user={user} handleLogout={handleLogout}/>
-      </div>
+    <>
+      <header>
+        <Header user={user} handleLogout={handleLogout} />
+      </header>
       <Outlet />
-    </div>
+    </>
   );
 }
 
