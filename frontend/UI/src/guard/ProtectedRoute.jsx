7379ec34d834ab9efacf4e5f75d8814d38a04d92
Arturo Escobar <s.arturoec@gmail.com>
2024-08-14 13:24:45 -0600

    update PretectedRoute guard to work with roles and any process

--- frontend/UI/src/guard/ProtectedRoute.jsx
+++ frontend/UI/src/guard/ProtectedRoute.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({
+  userRolesAllowed,
+  userProcessIdAllowed,
+  children,
+}) => {
   const { user, isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
     return <Navigate to="/auth" />;
   }
-  if (user.role !== "ADMIN") {
+  
+  if (userRolesAllowed.every((role) => user.role !== role)) {
     return <Navigate to="/unauthorized" />;
   }
+
+  if (user.processId !== userProcessIdAllowed) {
+    return <Navigate to="/unauthorized" />;
+  }
+
   return children;
 };
 
