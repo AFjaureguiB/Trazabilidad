a62f813b9d535b571c2f94e228c908c1623bf4a9
Arturo Escobar <s.arturoec@gmail.com>
2024-08-19 12:47:35 -0600

    add validation for an expired JWT, this way the error when starting the application is corrected

--- frontend/UI/src/context/AuthContext.jsx
+++ frontend/UI/src/context/AuthContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
+import { logout } from "../services/auth.service";
 
 const AuthContext = createContext();
 
@@ -12,8 +15,14 @@ export function AuthProvider({ children }) {
   const user = JSON.parse(localStorage.getItem("user")) || "";
   const isAuthenticated = user ? true : false;
 
+  const token = localStorage.getItem("accestkn") || "";
+  const currentTime = Math.floor(Date.now() / 1000); // Tiempo actual en segundos
+
+  const isValidToken = token ? jwtDecode(token).exp > currentTime : false;
+
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (!isAuthenticated || !isValidToken) {
+      logout();
       navigate("/auth");
       return;
     }
