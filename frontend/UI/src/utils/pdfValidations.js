fecba537a80ddef9d9f52c21db98fb6d9fc5bae5
Arturo Escobar <s.arturoec@gmail.com>
2024-09-01 16:43:21 -0600

    update pdf validations to avoid problems when editing a tissue

--- frontend/UI/src/utils/pdfValidations.js
+++ frontend/UI/src/utils/pdfValidations.js
@@ -1,8 +1,9 @@
 export const isPdf = (fileList) => {
-  if (!fileList || fileList.length === 0) return false;
+  if (!fileList || fileList.length === 0) return true;
   return fileList[0].type === "application/pdf" || "El archivo debe ser un PDF";
 };
 
 export const isSingleFile = (fileList) => {
+  if (!fileList || fileList.length === 0) return true;
   return fileList.length === 1 || "Solo se permite un archivo";
 };
