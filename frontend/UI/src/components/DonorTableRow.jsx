3d249fb073a337565491eb1119eab7b4d7883162
Arturo Escobar <s.arturoec@gmail.com>
2024-09-02 22:48:06 -0600

    add entry date of tissue in DonorTableRow

--- frontend/UI/src/components/DonorTableRow.jsx
+++ frontend/UI/src/components/DonorTableRow.jsx
@@ -94,99 +94,110 @@ export default function DonorTableRow({
         </td>
       </tr>
       {expanded
-        ? donor.Tissues.map((tissue) => (
-            <tr key={tissue.code} className="border-b">
-              <td colSpan={6}>
-                <div className="px-6 py-4">
-                  <div className="grid grid-cols-5 grid-rows-1 gap-4 items-center border border-blue-100 p-2 rounded-lg bg-blue-50/80">
-                    <div className="flex gap-2 items-center">
-                      <PuzzlePiece />
-                      <div className="space-y-1">
-                        <p className="text-lg font-bold">{tissue.tissuetype}</p>
-                        <p className="space-x-1 text-gray-500">
-                          <span>Codigo: </span>
-                          <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
-                            {tissue.code}
+        ? donor.Tissues.map((tissue) => {
+            const [createdAtDate] = tissue.createdAt.split("T");
+            return (
+              <tr key={tissue.code} className="border-b">
+                <td colSpan={6}>
+                  <div className="px-6 py-4">
+                    <div className="grid grid-cols-5 grid-rows-1 gap-4 items-center border border-blue-100 p-2 rounded-lg bg-blue-50/80">
+                      <div className="flex gap-2 items-center">
+                        <PuzzlePiece />
+                        <div className="space-y-1">
+                          <p className="text-lg font-bold">
+                            {tissue.tissuetype}
+                          </p>
+                          <p className="space-x-1 text-gray-500">
+                            <span>Codigo: </span>
+                            <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                              {tissue.code}
+                            </span>
+                          </p>
+                          <p className="space-x-1 text-gray-500">
+                            <span>Nevera:</span>
+                            <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                              {tissue.location}
+                            </span>
+                          </p>
+                          <p className="space-x-1 text-gray-500">
+                            <span>Ingreso:</span>
+                            <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                              {createdAtDate}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="space-y-1 text-right">
+                        <p className="text-gray-500 space-x-1">
+                          <span>IPS: </span>
+                          <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded whitespace-nowrap">
+                            {tissue.ips}
                           </span>
                         </p>
-                        <p className="space-x-1 text-gray-500">
-                          <span>Nevera:</span>
-                          <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
-                            {tissue.location}
+                        <p className="text-gray-500 space-x-1">
+                          <span>Recolecion: </span>
+                          <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded whitespace-nowrap">
+                            {tissue.collectedAt}
+                          </span>
+                        </p>
+                        <p>
+                          <span className="bg-indigo-100 text-indigo-800 text-xs font-medium px-2.5 py-0.5 rounded whitespace-nowrap">
+                            {tissue.specialistname}
                           </span>
                         </p>
                       </div>
-                    </div>
 
-                    <div className="space-y-1 text-right">
-                      <p className="text-gray-500 space-x-1">
-                        <span>IPS: </span>
-                        <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded whitespace-nowrap">
-                          {tissue.ips}
-                        </span>
-                      </p>
-                      <p className="text-gray-500 space-x-1">
-                        <span>Recolecion: </span>
-                        <span className="bg-indigo-100 text-indigo-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded whitespace-nowrap">
-                          {tissue.collectedAt}
-                        </span>
-                      </p>
-                      <p>
-                        <span className="bg-indigo-100 text-indigo-800 text-xs font-medium px-2.5 py-0.5 rounded whitespace-nowrap">
-                          {tissue.specialistname}
-                        </span>
-                      </p>
-                    </div>
+                      <div>
+                        <a
+                          href={`http://localhost:4000/consentimiento/${tissue.pdfpath}`}
+                          target="_blank"
+                          className="font-medium text-blue-600 hover:underline flex gap-1 justify-center"
+                        >
+                          <DocumentArrowDown />
+                          Consentimiento
+                        </a>
+                      </div>
 
-                    <div>
-                      <a
-                        href={`http://localhost:4000/consentimiento/${tissue.pdfpath}`}
-                        target="_blank"
-                        className="font-medium text-blue-600 hover:underline flex gap-1 justify-center"
-                      >
-                        <DocumentArrowDown />
-                        Consentimiento
-                      </a>
-                    </div>
-
-                    <div>
-                      <p className="text-pretty text-gray-500">
-                        {tissue.description
-                          ? tissue.description
-                          : "Sin descripcion adicional ..."}
-                      </p>
-                    </div>
-                    <div className="flex justify-center">
-                      <div className="space-y-3">
-                        <p className="space-x-1">
-                          <span className="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded">
-                            {tissue.status}
-                          </span>
+                      <div>
+                        <p className="text-pretty text-gray-500">
+                          {tissue.description
+                            ? tissue.description
+                            : "Sin descripcion adicional ..."}
                         </p>
-                        {user.role === userRoles.ADMIN ? (
-                          <button
-                            className="font-medium text-blue-600 flex gap-1 hover:underline"
-                            title="Editar informacion del tejido"
-                            onClick={() =>
-                              handleAddTissue(
-                                donor.id,
-                                donor.names,
-                                donor.surnames,
-                                tissue
-                              )
-                            }
-                          >
-                            <Pencil />
-                            <span>Editar tejido</span>
-                          </button>
-                        ) : null}
+                      </div>
+                      <div className="flex justify-center">
+                        <div className="space-y-3">
+                          <p className="space-x-1">
+                            <span className="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded">
+                              {tissue.status}
+                            </span>
+                          </p>
+                          {user.role === userRoles.ADMIN ? (
+                            <button
+                              className="font-medium text-blue-600 flex gap-1 hover:underline"
+                              title="Editar informacion del tejido"
+                              onClick={() =>
+                                handleAddTissue(
+                                  donor.id,
+                                  donor.names,
+                                  donor.surnames,
+                                  tissue
+                                )
+                              }
+                            >
+                              <Pencil />
+                              <span>Editar tejido</span>
+                            </button>
+                          ) : null}
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </td>
-            </tr>
-          ))
+                </td>
+              </tr>
+            );
+          })
         : null}
     </>
   );
