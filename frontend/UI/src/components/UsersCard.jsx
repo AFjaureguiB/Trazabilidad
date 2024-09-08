c3a270ddd9e192bac518f9fc0542cc5f104ec182
Arturo Escobar <s.arturoec@gmail.com>
2024-09-08 13:31:39 -0600

    add fallback content in case there are no users

--- frontend/UI/src/components/UsersCard.jsx
+++ frontend/UI/src/components/UsersCard.jsx
@@ -14,60 +14,73 @@ export default function UsersCard({
         </h5>
       </div>
       <div className="">
-        <ul role="list" className="divide-y divide-gray-200 ">
-          {users.map((user) => (
-            <li className="py-3 sm:py-4 divide-y divide-gray-200" key={user.id}>
-              <div
-                className={`flex items-center px-4 py-3 rounded-lg ${
-                  user.id === idUserEditing ? "bg-slate-200/80" : ""
-                }`}
+        {!users ? (
+          <div
+            className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50"
+            role="alert"
+          >
+            <span className="font-medium">Hey!</span> No tienes usuarios
+            creados.
+          </div>
+        ) : (
+          <ul role="list" className="divide-y divide-gray-200 ">
+            {users.map((user) => (
+              <li
+                className="py-3 sm:py-4 divide-y divide-gray-200"
+                key={user.id}
               >
-                <div className="flex-shrink-0">
-                  <span className="relative size-9 overflow-hidden bg-gray-100 rounded-full block">
-                    <svg
-                      className="absolute w-11 h-10 text-gray-400 -left-1"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
+                <div
+                  className={`flex items-center px-4 py-3 rounded-lg ${
+                    user.id === idUserEditing ? "bg-slate-200/80" : ""
+                  }`}
+                >
+                  <div className="flex-shrink-0">
+                    <span className="relative size-9 overflow-hidden bg-gray-100 rounded-full block">
+                      <svg
+                        className="absolute w-11 h-10 text-gray-400 -left-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-base font-medium text-gray-900 truncate ">
+                      {user.firstname} {user.lastname}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate ">
+                      {user.email}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate ">
+                      {user.username}
+                    </p>
+                  </div>
+                  <div className="flex  gap-3">
+                    <button
+                      className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                      title="Editar informacion de usuario"
+                      onClick={() => handleEdit(user)}
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </span>
+                      <Pencil />
+                    </button>
+                    <button
+                      className="px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"
+                      title="Eliminar usuario"
+                      onClick={() => handleDelete(user.id)}
+                    >
+                      <Trash />
+                    </button>
+                  </div>
                 </div>
-                <div className="flex-1 min-w-0 ms-4">
-                  <p className="text-base font-medium text-gray-900 truncate ">
-                    {user.firstname} {user.lastname}
-                  </p>
-                  <p className="text-sm text-gray-500 truncate ">
-                    {user.email}
-                  </p>
-                  <p className="text-sm text-gray-500 truncate ">
-                    {user.username}
-                  </p>
-                </div>
-                <div className="flex  gap-3">
-                  <button
-                    className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                    title="Editar informacion de usuario"
-                    onClick={() => handleEdit(user)}
-                  >
-                    <Pencil />
-                  </button>
-                  <button
-                    className="px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300"
-                    title="Eliminar usuario"
-                    onClick={() => handleDelete(user.id)}
-                  >
-                    <Trash />
-                  </button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
