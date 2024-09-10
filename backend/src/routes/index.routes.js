ac14673adfc475efd22dc20f33daeb19b42af78d
AFjaureguiB <ajauregui@unab.edu.co>
2024-09-10 17:32:42 -0500

    adding the functionality to consult the logs info and adding the route to te main router index

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -17,9 +17,6 @@ import logRoutes from "./logs.routes.js";
 
 import infectiousTestRoutes from "./infectiousTest.routes.js";
 
-// Importa el archivo de rutas de logs
-import logRoutes from "./logs.routes.js";
-
 /** Middleware de autenticación */
 import authenticationMiddleware from "../middlewares/authentication.middleware.js";
 
@@ -52,8 +49,7 @@ router.use(
   "/sterilization-batches",
   authenticationMiddleware,
   sterilizationBatchRoutes
-); // Define la ruta para consultar los logs /api/logs
-router.use("/logs", authenticationMiddleware, logRoutes);
+);
 
 // Exporta el enrutador
 export default router;
