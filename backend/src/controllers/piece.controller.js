004e9bd1c408922fb6e76e5efd1b3de375a80176
Arturo Escobar <s.arturoec@gmail.com>
2024-10-03 00:06:28 -0600

    add missing import respondSuccess function

--- backend/src/controllers/piece.controller.js
+++ backend/src/controllers/piece.controller.js
@@ -1,11 +1,12 @@
 "use strict";
 import PieceService from "../services/piece.service.js";
 import { handleError } from "../utils/errorHandler.js";
-import { respondError } from "../utils/resHandler.js";
+import { respondError, respondSuccess } from "../utils/resHandler.js";
 
 async function savePiece(req, res) {
   try {
     const { body } = req;
+    console.log(body);
     const { tissueId, ...piece } = body;
 
     //TODO: Validar con un schema de Joi o Zod el cuerpo de la request
