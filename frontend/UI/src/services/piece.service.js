88555fcd0b38afb93c4148fc6ef476979f3fab89
Arturo Escobar <s.arturoec@gmail.com>
2024-10-05 19:11:40 -0600

    add updatePiece function service to update piece data

--- frontend/UI/src/services/piece.service.js
+++ frontend/UI/src/services/piece.service.js
@@ -1,4 +1,5 @@
 import axios from "../services/root.service";
+import { referencias } from "../constants/referencias";
 
 export const savePiece = async (payload) => {
   try {
@@ -6,13 +7,14 @@ export const savePiece = async (payload) => {
     if (!token) return;
 
     const code = `${payload.code}-${payload.letter}`;
+    const description = referencias[payload.references];
     const pieceData = {
       tissueId: payload.tissueId,
       code,
       references: payload.references,
-      description: payload.description,
+      description,
     };
-
+    console.log(pieceData);
     const response = await axios.post("/pieces", pieceData, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -21,6 +23,36 @@ export const savePiece = async (payload) => {
 
     const { status, data } = response;
     if (status === 201) return data;
+  } catch (error) {
+    console.log(error);
+    return error.response.data;
+  }
+};
+
+export const updatePiece = async (payload) => {
+  try {
+    const token = localStorage.getItem("accestkn");
+    if (!token) return;
+
+    const code = `${payload.code}-${payload.letter}`;
+    const description = referencias[payload.references];
+
+    const pieceData = {
+      id: payload.id,
+      code,
+      references: payload.references,
+      description,
+    };
+
+    console.log(pieceData);
+
+    const response = await axios.put("/pieces", pieceData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { status, data } = response;
+    if (status === 200) return data;
   } catch (error) {
     console.log(error);
     return error.response.data;
