77f39d4a8792437466277d28137b7cbd8566bbee
Arturo Escobar <s.arturoec@gmail.com>
2024-11-11 23:08:35 -0600

    add LogsTableAdministration component and add tabs to show data in  Administration page

---
+++ frontend/UI/src/components/LogsTableAdministration.jsx
@@ -0,0 +1,56 @@
+import { useCallback } from "react";
+
+/* eslint-disable react/prop-types */
+export default function LogsTableAdministration({ logs }) {
+  // Función para formatear los datos de "Datos"
+  const formatUpdatedData = useCallback((data) => {
+    return Object.entries(data)
+      .map(([key, value]) => {
+        return `${key}: Se cambió ${value.prevValue} por ${value.newValue}`;
+      })
+      .join("\n"); // Utilizamos un salto de línea para que cada dato esté en una nueva línea
+  }, []);
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th className="px-6 py-3">Fecha</th>
+            <th className="px-6 py-3">Tipo</th>
+            <th className="px-6 py-3">Usuario</th>
+            <th className="px-6 py-3">Rol</th>
+            <th className="px-6 py-3">Proceso</th>
+            <th className="px-6 py-3">Acción</th>
+            <th className="px-6 py-3">Datos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.length > 0 ? (
+            logs.map((log, index) => (
+              <tr key={index} className="bg-white border-b">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {log.timestamp}
+                </td>
+                <td className="px-6 py-4">{log.info}</td>
+                <td className="px-6 py-4">{log.user}</td>
+                <td className="px-6 py-4">{log.role}</td>
+                <td className="px-6 py-4">{log.process}</td>
+                <td className="px-6 py-4">{log.action}</td>
+                <td className="px-6 py-4 whitespace-pre-wrap">
+                  {formatUpdatedData(log.updatedData)}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="7" className="text-center py-4 text-gray-500">
+                No logs available
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- frontend/UI/src/routes/Administration.jsx
+++ frontend/UI/src/routes/Administration.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getLogs } from "../services/logs.service"; // Importa el servicio para obtener los logs
+import LogsTableAdministration from "../components/LogsTableAdministration";
+import Tabs from "../components/Tabs.jsx";
 
 const Administration = () => {
   const [logs, setLogs] = useState([]);
@@ -13,63 +15,14 @@ const Administration = () => {
     setLogs(res); // Establece los logs en el estado
   };
 
-  // Función para formatear los datos de "Datos"
-  const formatUpdatedData = (data) => {
-    return Object.entries(data)
-      .map(([key, value]) => {
-        return `${key}: Se cambió ${value.prevValue} por ${value.newValue}`;
-      })
-      .join("\n"); // Utilizamos un salto de línea para que cada dato esté en una nueva línea
-  };
-
   return (
-    <div className=" relative flex items-center justify-center p-6">
-      <div className="flex items-center justify-center rounded-xl border-red-500 border-2">
-        <h2 className="text-2xl font-bold mb-6 text-gray-800 dark:text-white">
-          Logs
-        </h2>
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th className="px-6 py-3">Fecha</th>
-              <th className="px-6 py-3">Tipo</th>
-              <th className="px-6 py-3">Usuario</th>
-              <th className="px-6 py-3">Rol</th>
-              <th className="px-6 py-3">Proceso</th>
-              <th className="px-6 py-3">Acción</th>
-              <th className="px-6 py-3">Datos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {logs.length > 0 ? (
-              logs.map((log, index) => (
-                <tr
-                  key={index}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                >
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {log.timestamp}
-                  </td>
-                  <td className="px-6 py-4">{log.info}</td>
-                  <td className="px-6 py-4">{log.user}</td>
-                  <td className="px-6 py-4">{log.role}</td>
-                  <td className="px-6 py-4">{log.process}</td>
-                  <td className="px-6 py-4">{log.action}</td>
-                  <td className="px-6 py-4 whitespace-pre-wrap">
-                    {formatUpdatedData(log.updatedData)}
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="7" className="text-center py-4 text-gray-500">
-                  No logs available
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+    <div className="mt-4">
+      <Tabs>
+        <Tabs.Item title="Logs">
+          <LogsTableAdministration logs={logs} />
+        </Tabs.Item>
+        <Tabs.Item title="Usuarios Admin"></Tabs.Item>
+      </Tabs>
     </div>
   );
 };
