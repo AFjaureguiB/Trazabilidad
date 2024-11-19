3959fb5dce8e2a7acfd4981bf435e3b65aa384df
Arturo Escobar <s.arturoec@gmail.com>
2024-11-19 00:53:08 -0600

    add contact mail to recovery password

--- frontend/UI/src/components/LoginForm.jsx
+++ frontend/UI/src/components/LoginForm.jsx
@@ -7,7 +7,7 @@ function LoginForm() {
   const navigate = useNavigate();
 
   const [feedBack, setFeedBack] = useState("");
-
+  const [showRecoveryPassMessage, setShowRecoveryPassMessage] = useState(false);
   const {
     register,
     handleSubmit,
@@ -19,28 +19,23 @@ function LoginForm() {
 
     if (status === 400) setFeedBack(message);
     if (status === 200) navigate("/");
-    /*login(data).then((res) => {
-      console.log(res);
-      setFeedBack(res.message);
-      navigate("/");
-    }); */
   };
 
   return (
     <div className="relative h-screen flex items-center justify-center">
-      {/* Imagen de fondo con blur */}
       <div
         className="absolute inset-0 bg-cover bg-center blur-[4px] z-0"
         style={{ backgroundImage: `url('/background.jpg')` }}
       ></div>
 
-      {/* Overlay oscuro sobre la imagen de fondo */}
       <div className="absolute inset-0 bg-black/55 z-0"></div>
 
-      {/* Contenido principal */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between w-9/10 gap-12 bg-slate-100/60 px-6 rounded-xl shadow-2xl">
-        <img src="/logoFHD.png" className="w-1/2 h-auto object-contain mr-1" />
-        <div className="p-10 rounded-xl flex flex-col items-center justify-center w-full md:w-1/2 bg-trasnparent">
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between w-9/10 gap-12 bg-slate-100/60 p-8 rounded-xl shadow-2xl">
+        <img
+          src="/logoFHD.png"
+          className="w-1/2 h-auto object-contain mr-1 max-w-xl"
+        />
+        <div className="rounded-xl flex flex-col items-center justify-center w-full md:w-1/2 bg-trasnparent">
           {feedBack && (
             <div
               className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 w-full"
@@ -49,58 +44,80 @@ function LoginForm() {
               <span className="font-medium">Oops!</span> {feedBack}
             </div>
           )}
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-full flex flex-col space-y-4"
-          >
-            {/* Campos de entrada */}
-            <div>
-              <label
-                htmlFor="username"
-                className="block mb-2 text-sm font-medium text-black"
+          {!showRecoveryPassMessage ? (
+            <>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="w-full flex flex-col space-y-4"
               >
-                Username:
-              </label>
-              <input
-                id="username"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                name="username"
-                required
-                type="text"
-                {...register("username", { required: true })}
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-black"
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-black"
+                  >
+                    Username:
+                  </label>
+                  <input
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    name="username"
+                    required
+                    type="text"
+                    {...register("username", { required: true })}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-black"
+                  >
+                    Password:
+                  </label>
+                  <input
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    type="password"
+                    name="password"
+                    required
+                    {...register("password", { required: true })}
+                  />
+                </div>
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input
+                  type="submit"
+                  className="text-white w-full bg-[#183254] hover:bg-[#2785c7] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  value="Ingresar"
+                />
+              </form>
+              <button
+                className="font-medium text-blue-600 hover:underline mt-4"
+                onClick={() => setShowRecoveryPassMessage(true)}
               >
-                Password:
-              </label>
-              <input
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                type="password"
-                name="password"
-                required
-                {...register("password", { required: true })}
-              />
+                ¿Olvidaste tu contraseña?
+              </button>
+            </>
+          ) : (
+            <div className="max-w-md">
+              <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900">
+                Contacta a{" "}
+                <span className="text-blue-600">mail@example.com</span> para
+                recuperar tu contraseña
+              </h1>
+              <button
+                className="font-medium text-xl text-blue-600 hover:underline"
+                onClick={() => setShowRecoveryPassMessage(false)}
+              >
+                Inicia Sesion
+              </button>
             </div>
-            {errors.exampleRequired && <span>This field is required</span>}
-            <input
-              type="submit"
-              className="text-white w-full bg-[#183254] hover:bg-[#2785c7] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-              value="Ingresar"
-            />
-          </form>
+          )}
         </div>
       </div>
 
-      {/* Footer en la esquina inferior izquierda */}
       <footer className="absolute bottom-0 left-0 z-10 bg-transparent">
         <p className="text-left">
           <a
-            href="https://www.fundonemos.org" // Cambia esto por la URL que desees
+            href="https://www.fundonemos.org"
             className="text-white hover:underline"
             target="_blank"
             rel="noopener noreferrer"
