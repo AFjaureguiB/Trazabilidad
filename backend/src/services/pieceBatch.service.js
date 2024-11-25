9b2a325cb78ea0936e1e28ae1a8b002f82d67188
Arturo Escobar <s.arturoec@gmail.com>
2024-11-24 20:30:23 -0600

    replace "reactivo|no reactivo" by "aprobado|no aprobado" in chemical test result

--- backend/src/services/pieceBatch.service.js
+++ backend/src/services/pieceBatch.service.js
@@ -109,11 +109,11 @@ export async function updatePieceBatchStatusAccordingChemicalTests(
       p.chemicalTests.slice(0, 1)
     );
 
-    // Si alguna prueba es "Reactivo", el estado es "rechazado"
-    const status = chemicalTests.some((test) => test.result === "Reactivo")
+    // Si alguna prueba es "No Aprobado", el estado es "rechazado"
+    const status = chemicalTests.some((test) => test.result === "No Aprobado")
       ? "Rechazado"
       : chemicalTests.length === 3 &&
-        chemicalTests.every((test) => test.result === "No Reactivo")
+        chemicalTests.every((test) => test.result === "Aprobado")
       ? "Pre-Aprobado"
       : "Stand By";
 
