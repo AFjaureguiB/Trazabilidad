738c6b1f70ac09b1cda636f0602063bf7b04592a
AFjaureguiB <ajauregui@unab.edu.co>
2024-12-01 19:49:17 -0500

    update pdf function to display more professional document

--- backend/src/services/dispatchpdf.service.js
+++ backend/src/services/dispatchpdf.service.js
@@ -3,30 +3,73 @@ import { handleError } from "../utils/errorHandler.js";
 
 async function buildDispatchPDF(dataCallback, endCallback, shipment) {
   try {
-    const doc = new PDFDocument();
+    const doc = new PDFDocument({ margin: 30 });
 
     doc.on("data", dataCallback);
     doc.on("end", endCallback);
 
-    //Si quieres agregar mas informacion, en el siguiente objeto lo puedes hacer
-    //El objeto `shipment` ya tiene toda la informacion que se muestra en el front, solo tomala y muestrala en el PDF
+    // Añadir el logo SVG
+    doc.image("public/logoFHD.png", {
+      fit: [200, 250],
+      align: "left",
+      valign: "left",
+    });
+
+    // Añadir información del encabezado
+    doc
+      .fontSize(12)
+      .fillColor("#183254")
+      .text("Fundonemos Banco de Tejidos", 140, 35, { align: "right" })
+      .fontSize(10)
+      .fillColor("#000000")
+      .text(`Parque industrial de Bucarmanga`, 140, 50, { align: "right" })
+      .fontSize(10)
+      .text(`Manzana C - Bodega 12`, 140, 65, { align: "right" })
+      .fontSize(10)
+      .text(`Orden No.: ${shipment.id}`, 140, 80, { align: "right" })
+      .moveDown();
+
+    // Información del paciente
+    doc
+      .fontSize(10)
+      .text(`Fecha de impresión: ${new Date().toLocaleString()}`, 30, 125, {
+        align: "left",
+      })
+      .text(`Paciente: ${shipment.patientname}`, 30, 140)
+      .text(`Documento: ${shipment.patientdni}`, 30, 155)
+      .text(`Fecha de envío: ${shipment.shippingdate}`, 30, 170)
+      .moveDown();
+
+    doc
+      .moveTo(20, 10) // Punto superior izquierdo (x: 50, y: 150)
+      .roundedRect(20, 20, 575, 95, 10) // Rectángulo con bordes redondeados (ancho: 200, alto: 100, radio de esquina: 20)
+      .stroke(); // Dibuja el rectángulo
+
+    // Crear la tabla
     const table = {
-      title: "Envio",
-      subtitle: "Realizado",
-      headers: ["Numero Envio", "Total de Piezas", "Info IPS", "Info Paciente"],
+      title: "Información del envío",
+
+      subtitle: "Detalles",
+
+      headers: [
+        "Número de Envío",
+        "Total de Piezas",
+        "Info IPS",
+        "Info Paciente",
+      ],
       rows: [
         [
           shipment.id,
           shipment.quantity,
-          `IPS Receptora: ${shipment.receivingips}\n Sede: ${shipment.sede}\n Fecha de envio: ${shipment.shippingdate}`,
-          `Nombre Paciente: ${shipment.patientname}\n DNI: ${shipment.patientdni}\n Nombre Especialista: ${shipment.specialistname}`,
+          `IPS Receptora: ${shipment.receivingips}\nSede: ${shipment.sede}\nFecha de envío: ${shipment.shippingdate}`,
+          `Nombre Paciente: ${shipment.patientname}\nDNI: ${shipment.patientdni}\nNombre Especialista: ${shipment.specialistname}`,
         ],
       ],
     };
 
-    doc.fontSize(25).text("Orden de Envio de Piezas");
+    doc.moveDown();
     await doc.table(table, {
-      columnsSize: [50, 50, 200, 200],
+      columnsSize: [70, 70, 200, 200],
     });
 
     doc.end();
